441879ff7cebec19b3317145f3188eded1d56c35
MonsterComputer <MonsterComputer@DESKTOP-HP6TDQG>
2019-12-20 09:58:07 +0100

    added a comment

--- banggood.com scraper/Program.cs
+++ banggood.com scraper/Program.cs
@@ -11,6 +11,7 @@ namespace banggood.com_scraper
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// //comment
         [STAThread]
         static void Main()
         {
